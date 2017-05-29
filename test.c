0bf6007e4c454f1000be621c31a677a7f475f65f
JaeIkLee <21200574@handong.edu>
2017-05-30 02:38:36 +0900

    lucky 7

--- test.c
+++ test.c
@@ -3,21 +3,23 @@
 
 int calculate_result(int white_balls[5], int power_ball)
 {
-	return 0;
-for (int i=0; i<5; i++)
-    {
-	if ((white_balls[i] < 1) || (white_balls[i] > 59))
-	{
-	return -1;
-        } 
-    }
+
+    for (int i=0; i<5; i++)
+        {
+	    if ((white_balls[i] < 1) || (white_balls[i] > 59))
+	    { 
+            	return -1;
+            } 
+        }
+
     if ((power_ball < 1)
          || (power_ball > 39))
 	{
 	return -1;
 	}
 
-    }  
+    return 0;	
+}  
 
 int main(int argc, char** argv)
 {
@@ -35,6 +37,11 @@ int main(int argc, char** argv)
 	}
     	
 	int result = calculate_result(white_balls, power_ball);
+	
+	if (7 == power_ball)
+	{
+		result = result * 2;
+	}
 
     	printf("%d percent chance of winning\n", result);
 
